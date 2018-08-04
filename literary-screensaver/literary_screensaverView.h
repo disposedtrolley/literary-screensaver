b661fedfe53b4dfe3d8f02d33980a21b6d4930e6
jamesliu <jamesliu@au1.ibm.com>
2018-08-05 09:38:06 +1000

    Initial Commit

---
+++ literary-screensaver/literary_screensaverView.h
@@ -0,0 +1,13 @@
+//
+//  literary_screensaverView.h
+//  literary-screensaver
+//
+//  Created by James Liu on 5/8/18.
+//  Copyright © 2018 James Liu. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface literary_screensaverView : ScreenSaverView
+
+@end
