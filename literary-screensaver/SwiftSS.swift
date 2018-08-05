2ac5a7f03861721fde20995e86d0e1533cdc38a8
jamesliu <jamesliu@au1.ibm.com>
2018-08-05 10:26:58 +1000

    docs: add function docs

--- literary-screensaver/SwiftSS.swift
+++ literary-screensaver/SwiftSS.swift
@@ -1,11 +1,3 @@
-//
-//  SwiftSS.swift
-//  literary-screensaver
-//
-//  Created by James Liu on 5/8/18.
-//  Copyright © 2018 James Liu. All rights reserved.
-//
-
 import ScreenSaver
 
 class SwiftSS: ScreenSaverView {
@@ -13,16 +5,30 @@ class SwiftSS: ScreenSaverView {
     var timer = Timer()
     var currentTime: String = ""
     
+    /**
+     startAnimation is called after the display has faded into the screen saver, and
+     kicks off the timer which updates the current time every second.
+     */
     override func startAnimation() {
         super.startAnimation()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
     }
     
+    /**
+     animateOneFrame is called every time the screen saver frame is to be updated, and
+     is used to re-draw the time/quote if required.
+     
+     @TODO implement logic to determine if a re-draw is required.
+     */
     override func animateOneFrame() {
         clearStage()
         drawTime()
     }
     
+    /**
+     updateTime updates the "currentTime" instance variable with the current time
+     as a formatted string.
+     */
     func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
@@ -31,16 +37,26 @@ class SwiftSS: ScreenSaverView {
         currentTime = formatter.string(from: date)
     }
     
+    /**
+     drawTime draws the current time as a formatted string onto the screen saver
+     stage.
+     */
     func drawTime() {
         NSColor.black.set()
         currentTime.draw(at: NSPoint(x: 100.0, y: 200.0), withAttributes: nil)
     }
     
+    /**
+     clearStage clears the stage, by filling it with a solid colour.
+     */
     func clearStage() {
         NSColor.red.setFill()
         NSRectFill(self.bounds)
     }
     
+    /**
+     draw is called to initialise the stage of the screen saver.
+     */
     override func draw(_ rect: NSRect) {
         super.draw(rect)
         
